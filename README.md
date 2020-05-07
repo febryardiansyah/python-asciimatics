605ac9fad2702e275aaaff87e4559b6b925b80fe
febryardiansyah <febrymuhammad80@gmail.com>
2020-05-07 22:55:17 +0700

    kimi no nawa

--- README.md
+++ README.md
@@ -2,9 +2,12 @@
 show image animation gif, png, jpg in python
 
 # Instalation
-1. pip install asciimatics (install library)
+1. pip install asciimatics (https://pypi.org/project/asciimatics/)
 2. run main.py in terminal
 
 # Screenshoot
 <img src="ss1.png" width ="30%"><br>
-<img src="ss2.png" width ="30%">
+<img src="ss2.png" width ="30%"><br>
+<img src="ss3.png" width ="30%"><br>
+<img src="ss4.png" width ="30%"><br>
+<img src="ss5.png" width ="30%"><br>
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from __future__ import division
-from asciimatics.effects import BannerText, Print, Scroll
+from asciimatics.effects import BannerText, Print, Scroll ,Cycle, Stars
 from asciimatics.renderers import ColourImageFile, FigletText, ImageFile
 from asciimatics.scene import Scene
 from asciimatics.screen import Screen
@@ -8,37 +8,70 @@ import sys
 
 
 def demo(screen):
+    #empty array to save all effects
     scenes = []
+    #show text
     effects = [
         Print(screen,
-              FigletText("SKUYY",
+              FigletText("yourname",
                          font='banner3' if screen.width > 80 else 'banner'),
               screen.height//2-3,
               colour=7, bg=7 if screen.unicode_aware else 0),
     ]
     scenes.append(Scene(effects))
+    #show taki.gif
     effects = [
         Print(screen,
-              ColourImageFile(screen, "joget.gif", screen.height,
+              ColourImageFile(screen, "taki.gif", screen.height,
                               uni=screen.unicode_aware),
               screen.height,
               speed=1),
         Scroll(screen, 2)
     ]
     scenes.append(Scene(effects))
+    #show mitsuha.gif
     effects = [
         BannerText(screen,
-                   ColourImageFile(screen, "joget.gif", screen.height-2,
+                   ColourImageFile(screen, "mitsuha.gif", screen.height-2,
                                    uni=screen.unicode_aware, dither=screen.unicode_aware),
                    0, 0),
     ]
     scenes.append(Scene(effects))
+    #show mitshua2.gif
     effects = [
         Print(screen,
-              FigletText("ANJAYY",
-                         font='banner3' if screen.width > 80 else 'banner'),
-              screen.height//2-3,
-              colour=7, bg=7 if screen.unicode_aware else 0),
+            ColourImageFile(screen, "mitsuha2.gif",screen.height,
+                            uni=screen.unicode_aware),
+            screen.height,
+            speed=1,),
+        Scroll(screen,2)
+    ]
+    scenes.append(Scene(effects))
+    #show both.gif
+    effects = [
+        Print(screen,
+              ColourImageFile(screen, "both.gif", screen.height-2,
+                              uni=screen.unicode_aware,
+                              dither=screen.unicode_aware),
+              0,
+              stop_frame=200),
+        # Print(screen,
+        #       FigletText("ASCIIMATICS",
+        #                  font='banner3' if screen.width > 80 else 'banner'),
+        #       screen.height//2-3,
+        #       colour=7, bg=7 if screen.unicode_aware else 0),
+    ]
+    scenes.append(Scene(effects))
+    #show end text
+    effects = [
+        Cycle(
+            screen, FigletText("KimiNoNamaewa?", font="big"),
+            int(screen.height/2-8)
+        ),
+        Cycle(
+            screen, FigletText("YourName", font="small"),
+            int(screen.height/2+3),
+        ),
     ]
     scenes.append(Scene(effects))
     screen.play(scenes, stop_on_resize=True)
